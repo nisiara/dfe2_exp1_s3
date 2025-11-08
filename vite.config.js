81f0bc877a5d08ba14f4cc40376d92a75de472b3
Nicolas Silva <nic.silvaa@duocuc.cl>
2025-11-07 23:08:48 -0300

    Deploy intent

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/'
+  base: 'https://nisiara.github.io/dfe2_exp1_s3/'
 })
  
