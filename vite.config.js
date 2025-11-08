fce982113a9eda8942034c9aca782a68ec0049f5
Nicolas Silva <nic.silvaa@duocuc.cl>
2025-11-08 15:45:00 -0300

    Change base url

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://nisiara.github.io/dfe2_exp1_s3/"
+  base: "/dfe2_exp1_s3/"
 })
  
