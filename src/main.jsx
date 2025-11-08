0aadd12e0354f95d47e1063c0f34573dae0c73d4
Nicolas Silva <nic.silvaa@duocuc.cl>
2025-11-08 00:19:42 -0300

    Add Github workflow

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter as Router } from 'react-router-dom'
+// import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router>
+    <HashRouter>
       <App />
-    </Router>
-  </StrictMode>,
+    </HashRouter>
+  </StrictMode>
 )
